7f6ef91525d58e679edf7d7ea31d5fad774765a2
kunzzsim <simon.kunz@seznam.cz>
2026-02-10 16:23:09 +0100

    Initial commit

---
+++ src/main/java/cz/osu/kunz/springsecuritycomparison/SpringSecurityComparisonApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.kunz.springsecuritycomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityComparisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityComparisonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/osu/kunz/springsecuritycomparison/SpringSecurityComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.kunz.springsecuritycomparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityComparisonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
