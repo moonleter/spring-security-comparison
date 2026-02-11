dab75913738b847424063e92d8411961463e2bef
kunzzsim <simon.kunz@seznam.cz>
2026-02-11 17:15:20 +0100

    feat: impl GlobalExceptionHandling

--- src/main/java/cz/osu/kunz/springsecuritycomparison/exception/GlobalExceptionHandler.java
+++ src/main/java/cz/osu/kunz/springsecuritycomparison/exception/GlobalExceptionHandler.java
@@ -1,4 +1,70 @@
 package cz.osu.kunz.springsecuritycomparison.exception;
 
-public class GlobalExceptionHandler {
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Arrays;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return buildErrorResponse("Validation error", HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return buildErrorResponse("Validation error", HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(org.springframework.web.bind.MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return buildErrorResponse("Validation error", HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> handleAccessDeniedException(AccessDeniedException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllUncaughtException(Exception ex, WebRequest webRequest) {
+        log.error("Exception: {}", ex.getClass().getName());
+        log.error("Message: {}", ex.getMessage());
+        if (ex.getCause() != null) {
+            log.error("Cause: {}", ex.getCause().toString());
+        }
+        log.error("Stack trace: {}", Arrays.toString(ex.getStackTrace()));
+
+        return buildErrorResponse("Unknown error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public ResponseEntity<Object> buildErrorResponse(String message, HttpStatus httpStatus) {
+        return ResponseEntity.status(httpStatus).body(
+                new ErrorResponse(httpStatus.value(), message)
+        );
+    }
 }
